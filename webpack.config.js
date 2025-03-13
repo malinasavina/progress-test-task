78d34021c0f84c930cfadd74d7c1e23243487009
Alina <alinalalalaa@gmail.com>
2025-03-13 18:56:50 +0300

    Set initial project

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/scripts/main.js',
+  mode: process.env.NODE_ENV || 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle[contenthash].js',
+    clean: true
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader"
+      ],
+    }, {
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+      type: 'asset/resource'
+    }, {
+      test: /\.html$/,
+      use: ["html-loader"],
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    open: true
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+}

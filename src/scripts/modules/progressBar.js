6bb45e5c0d709e495d2a9032720d576484fee565
Alina <alinalalalaa@gmail.com>
2025-03-14 21:15:40 +0300

    Replace css animation with requestAnimationFrame

--- src/scripts/modules/progressBar.js
+++ src/scripts/modules/progressBar.js
@@ -2,29 +2,66 @@ document.addEventListener('DOMContentLoaded', () => {
   const circle = document.querySelector('.js-progress-circle');
   const input = document.querySelector('.js-progress-input');
   const animationToggle = document.querySelector('.js-animation-toggle');
+
   const radius = circle.r.baseVal.value;
   const circumference = 2 * Math.PI * radius;
 
   circle.style.strokeDasharray = `${circumference} ${circumference}`;
   circle.style.strokeDashoffset = circumference;
 
-  setProgress(input.value);
+  let startTime;
+  let animationFrameId;
+  let progress = +input.value / 100;
+  let isAnimationRunning = false;
+  const duration = 2000;
+
+  setProgress(progress);
 
   input.addEventListener('input', function() {
     let inputValue = isNaN(+this.value) ? 0 : Math.min(Math.max(+this.value, 0), 100);
+    progress = inputValue / 100;
 
-    setProgress(inputValue);
+    setProgress(progress, isAnimationRunning);
     this.value = inputValue;
   });
 
   animationToggle.addEventListener('change', function() {
     this.checked
-      ? circle.classList.add('progress__circle_animated')
-      : circle.classList.remove('progress__circle_animated');
+      ? startAnimation()
+      : stopAnimation();
   });
 
-  function setProgress(percent) {
-    let offset = circumference - percent / 100 * circumference;
+  function animateProgress(timeStamp) {
+    if (!startTime) startTime = timeStamp;
+    progress = (timeStamp - startTime) / duration ;
+
+    if (progress > 1) {
+      startTime = timeStamp;
+      progress = 0;
+    }
+
+    setProgress(progress, isAnimationRunning);
+
+    animationFrameId = requestAnimationFrame(animateProgress);
+  }
+
+  function startAnimation() {
+    if (!isAnimationRunning) {
+      isAnimationRunning = true;
+      startTime = null;
+      animationFrameId = requestAnimationFrame(animateProgress);
+    }
+  }
+
+  function stopAnimation() {
+    isAnimationRunning = false;
+    cancelAnimationFrame(animationFrameId);
+  }
+
+  function setProgress(progress, isAnimated) {
+    circle.style.transition = !isAnimated ? 'stroke-dashoffset 0.2s ease-in-out' : 'none';
+
+    let offset = circumference * (1 - progress);
     circle.style.strokeDashoffset = offset;
   }
 });

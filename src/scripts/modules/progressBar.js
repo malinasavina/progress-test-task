a6401ea2f1ab3c6b7e7b8792d3e8ea34543db8dd
Alina <alinalalalaa@gmail.com>
2025-03-14 23:32:42 +0300

    Create separate Progress class

--- src/scripts/modules/progressBar.js
+++ src/scripts/modules/progressBar.js
@@ -1,67 +1,84 @@
+class Progress {
+  constructor(container) {
+    this.container = container;
+    this.circle = container.querySelector('.js-progress-circle');
+    this.input = container.querySelector('.js-progress-input');
+    this.animationToggle = container.querySelector('.js-animation-toggle');
+
+    this.radius = this.circle.r.baseVal.value;
+    this.circumference = 2 * Math.PI * this.radius;
+
+    this.startTime = null;
+    this.animationFrameId = null;
+    this.progress = +this.input.value / 100;
+    this.isAnimationRunning = false;
+    this.duration = 2000;
+
+    this.init();
+  }
+
+  init() {
+    this.circle.style.strokeDasharray = `${this.circumference} ${this.circumference}`;
+    this.circle.style.strokeDashoffset = this.circumference;
+
+    this.setProgress(this.progress, this.isAnimationRunning);
+
+    this.input.addEventListener('input', this.updateFromInput.bind(this));
+    this.animationToggle.addEventListener('change', this.toggleAnimation.bind(this))
+  }
+
+  setProgress(progress, isAnimated) {
+    this.circle.style.transition = !isAnimated ? 'stroke-dashoffset 0.3s ease-in-out' : 'none';
+
+    let offset = this.circumference * (1 - progress);
+    this.circle.style.strokeDashoffset = offset;
+  }
+
+  updateFromInput() {
+    let inputValue = isNaN(+this.input.value) ? 0 : Math.min(Math.max(+this.input.value, 0), 100);
+    this.progress = inputValue / 100;
+
+    this.setProgress(this.progress, this.isAnimationRunning);
+    this.input.value = inputValue;
+  }
+
+  animateProgress(timeStamp) {
+    if (!this.startTime) this.startTime = timeStamp;
+    this.progress = (timeStamp - this.startTime) / this.duration ;
+
+    if (this.progress > 1) {
+      this.startTime = timeStamp;
+      this.progress = 0;
+    }
+
+    this.setProgress(this.progress, this.isAnimationRunning);
+    this.animationFrameId = requestAnimationFrame((time) => this.animateProgress(time));
+  }
+
+  startAnimation() {
+    if (!this.isAnimationRunning) {
+      this.isAnimationRunning = true;
+      this.startTime = null;
+      this.animationFrameId = requestAnimationFrame((time) => this.animateProgress(time));
+    }
+  }
+
+  stopAnimation() {
+    this.isAnimationRunning = false;
+    cancelAnimationFrame(this.animationFrameId);
+  }
+
+  toggleAnimation() {
+    this.animationToggle.checked
+      ? this.startAnimation()
+      : this.stopAnimation();
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const circle = document.querySelector('.js-progress-circle');
-  const input = document.querySelector('.js-progress-input');
-  const animationToggle = document.querySelector('.js-animation-toggle');
+  const progressContainers = document.querySelectorAll('.progress');
 
-  const radius = circle.r.baseVal.value;
-  const circumference = 2 * Math.PI * radius;
-
-  circle.style.strokeDasharray = `${circumference} ${circumference}`;
-  circle.style.strokeDashoffset = circumference;
-
-  let startTime;
-  let animationFrameId;
-  let progress = +input.value / 100;
-  let isAnimationRunning = false;
-  const duration = 2000;
-
-  setProgress(progress);
-
-  input.addEventListener('input', function() {
-    let inputValue = isNaN(+this.value) ? 0 : Math.min(Math.max(+this.value, 0), 100);
-    progress = inputValue / 100;
-
-    setProgress(progress, isAnimationRunning);
-    this.value = inputValue;
+  progressContainers.forEach(container => {
+    new Progress(container);
   });
-
-  animationToggle.addEventListener('change', function() {
-    this.checked
-      ? startAnimation()
-      : stopAnimation();
-  });
-
-  function animateProgress(timeStamp) {
-    if (!startTime) startTime = timeStamp;
-    progress = (timeStamp - startTime) / duration ;
-
-    if (progress > 1) {
-      startTime = timeStamp;
-      progress = 0;
-    }
-
-    setProgress(progress, isAnimationRunning);
-
-    animationFrameId = requestAnimationFrame(animateProgress);
-  }
-
-  function startAnimation() {
-    if (!isAnimationRunning) {
-      isAnimationRunning = true;
-      startTime = null;
-      animationFrameId = requestAnimationFrame(animateProgress);
-    }
-  }
-
-  function stopAnimation() {
-    isAnimationRunning = false;
-    cancelAnimationFrame(animationFrameId);
-  }
-
-  function setProgress(progress, isAnimated) {
-    circle.style.transition = !isAnimated ? 'stroke-dashoffset 0.2s ease-in-out' : 'none';
-
-    let offset = circumference * (1 - progress);
-    circle.style.strokeDashoffset = offset;
-  }
 });

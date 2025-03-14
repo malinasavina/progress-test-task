79eb838c6586b7017daa4090489ec3fa1717dc69
Alina <alinalalalaa@gmail.com>
2025-03-14 20:24:09 +0300

    Add initial controls functionality

--- src/scripts/modules/progressBar.js
+++ src/scripts/modules/progressBar.js
@@ -1,18 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   const circle = document.querySelector('.js-progress-circle');
   const input = document.querySelector('.js-progress-input');
+  const animationToggle = document.querySelector('.js-animation-toggle');
   const radius = circle.r.baseVal.value;
   const circumference = 2 * Math.PI * radius;
 
   circle.style.strokeDasharray = `${circumference} ${circumference}`;
   circle.style.strokeDashoffset = circumference;
 
+  setProgress(input.value);
+
   input.addEventListener('input', function() {
-    setProgress(this.value);
+    let inputValue = isNaN(+this.value) ? 0 : Math.min(Math.max(+this.value, 0), 100);
+
+    setProgress(inputValue);
+    this.value = inputValue;
+  });
+
+  animationToggle.addEventListener('change', function() {
+    this.checked
+      ? circle.classList.add('progress__circle_animated')
+      : circle.classList.remove('progress__circle_animated');
   });
 
   function setProgress(percent) {
-    const offset = circumference - percent / 100 * circumference;
+    let offset = circumference - percent / 100 * circumference;
     circle.style.strokeDashoffset = offset;
   }
 });
